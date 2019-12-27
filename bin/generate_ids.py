1f78936dd4cd4e1aedde739ba3f8eb9097d43b9e
Hillel Arnold <helrond@hotmail.com>
2019-12-27 12:40:08 -0500

    adds script to generate ids

---
+++ bin/generate_ids.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+""" Generates unique identifiers for repositories
+
+    Usage:
+    python generate_ids.py
+"""
+
+import csv
+import shortuuid
+
+from os import listdir
+from os.path import join, dirname, abspath
+
+source_path = join(abspath(dirname(__file__)), '..', 'data.csv')
+out_path = join(abspath(dirname(__file__)), '..', 'data_ids.csv')
+
+shortuuid.set_alphabet("23456789abcdefghijkmnopqrstuvwxyz")
+
+def generate_ids():
+    with open(source_path, 'r') as source_file, open(out_path, 'w') as out_file:
+        csvreader = csv.DictReader(source_file)
+        fieldnames = ['ID'] + csvreader.fieldnames if "ID" not in csvreader.fieldnames else csvreader.fieldnames
+        csvwriter = csv.DictWriter(out_file, fieldnames, extrasaction='ignore')
+        csvwriter.writeheader()
+        for node, row in enumerate(csvreader, 1):
+            identifier = row.get("ID") if row.get("ID") else shortuuid.uuid()
+            csvwriter.writerow(dict(row, ID=identifier))
+
+if __name__ == "__main__":
+    generate_ids()
