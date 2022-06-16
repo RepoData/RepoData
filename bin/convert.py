33ea8f9451ff3ef8ed226f8b96b6a6175218080c
Hillel Arnold <helrond@hotmail.com>
2022-06-16 15:44:14 -0400

    replace empty strings with None

--- bin/convert.py
+++ bin/convert.py
@@ -6,10 +6,16 @@ import json
 def convert():
     repos = []
     for row in csv.DictReader(open('data.csv')):
-        repos.append(row)
+        repos.append(filter_row(row))
     json.dump(repos, open('data.json', 'w'), indent=2)
+
+def filter_row(row):
+    """Filters row data.
+
+    Converts empty strings to none.
+    """
+    filtered_row = {k: None if not v else v for k, v in row.items()}
+    return filtered_row
 
 if __name__ == "__main__":
     convert()
-
-
